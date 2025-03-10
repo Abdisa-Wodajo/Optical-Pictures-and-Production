919acd6abcf777df281424c34d6ae6637ce04f11
Abdisa-Wodajo <abdisawodajo41@gmail.com>
2025-03-10 23:22:42 +0300

    I added styles

--- Login/index.php
+++ Login/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
    <div class="container">
